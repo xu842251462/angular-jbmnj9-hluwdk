df9c43938010d91406e9925d500655eea85f5020
xu842251462 <xu842251462@gmail.com>
2019-11-24 15:23:15 -0500

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { products } from "../products";
 
+
+
 @Component({
   selector: "app-product-details",
   templateUrl: "./product-details.component.html",
